7a5b3565437d239b2f721a89c085f00b41981530
cff008 <cfoley3@wisc.edu>
2016-03-16 17:01:19 -0500

    First Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+     
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('signupCtrl', function($scope) {
+
+})
+   
+.controller('organizationProfileCtrl', function($scope) {
+
+})
+   
+.controller('myEventsCtrl', function($scope) {
+
+})
+   
+.controller('settingsCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,65 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/page5',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/page6',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('tabsController.organizationProfile', {
+    url: '/Profile',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/organizationProfile.html',
+        controller: 'organizationProfileCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.myEvents', {
+    url: '/MyEvents',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/myEvents.html',
+        controller: 'myEventsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.settings', {
+    url: '/Settings',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/settings.html',
+        controller: 'settingsCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/page5')
+
+  
+
+});
